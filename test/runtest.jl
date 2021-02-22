7e79a6ab0b994b5937123742f3acc2883f79e1f7
Yunlong Lian <algorithm.lian@gmail.com>
2021-02-22 11:28:51 +0800

    test

--- test/runtest.jl
+++ test/runtest.jl
@@ -1,26 +1,33 @@
-include("/home/dabajabaza/jianguoyun/Workspace/SmartParser/src/SmartParser.jl")
+using Pkg
+Pkg.activate("/home/dabajabaza/jianguoyun/Workspace/SmartParser/")
+using SmartParser
 
-
-##
+#
 
 #fn = "/data/ReO3_phonon_configs/ps_GBRV_PBE_kp_8,8,8,1,1,1_kBar_0.0_dg_0.02/ReO3_scf/ReO3.pw.x.out" ;
 #fn = "/data/ScF3_phonon_configs/ps_GBRV_PBE_kp_12,12,12,1,1,1_kBar_0.5_dg_0.02/ScF3_relax/ScF3.pw.x.out" ;
 fn = "/data/ScF3_phonon_configs/ps_GBRV_LDA_kp_12,12,12,1,1,1_kBar_0.0_dg_0.02/ScF3_relax/ScF3.pw.x.out" ;
 #fn = "./src/test_OUTP.txt";
-S0 = read(fn,String) ;
-patts, code = tokenize(mask(S0)) ;
-lines = split(S0,"\n") ;
-lines1 = split(mask(S0),"\n") ;
+
+lines, lines1, patts, code = load_file(fn) ;
 
 ##
 
 B0 = build_block_init(patts) ;
-B  = find_block(B0) ;
-NL1 = block_print(B, lines1, offset=0, mute=true) ;
+
+B1  = find_block(B0) ;
+println(max_depth(B1))
+
+B2  = loop_correct_1_1_until_stable(B1) ;
+println(max_depth(B2))
+
+
+NL = block_print(B2, lines1, offset=0, mute=true) ;
 
 open("test000.txt","w") do f
-    write(f,join(NL1,"\n"))
+    write(f,join(NL,"\n"))
 end
 
 ##
 
+findmax([length(children(c)) for c in children(B2)])
